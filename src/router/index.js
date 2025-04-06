f48cb79d3a221f95749289fabdf7949d4da924aa
luzticona7 <chaconlu68@gmail.com>
2025-04-05 23:23:12 -0400

    vistas

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
-
 import HomeView from '../views/HomeView.vue';
 import LoginView from '../views/LoginView.vue';
 import RegistroView from '../views/RegistroView.vue';
+import ControlAsistencia from '../views/controlAsistencia.vue';
+import HistorialAcademico from '../views/historialAcademico.vue';
 
 const routes = [
-  { path: '/', name: 'Home', component: HomeView }, 
-  { path: '/login', name: 'Login', component: LoginView }, 
+  { path: '/', name: 'Home', component: HomeView },
+  { path: '/login', name: 'Login', component: LoginView },
   { path: '/registro', name: 'Registro', component: RegistroView },
+  { path: '/control-asistencia', name: 'ControlAsistencia', component: ControlAsistencia },
+  { path: '/historial-academico', name: 'HistorialAcademico', component: HistorialAcademico }
 ];
 
 const router = createRouter({
