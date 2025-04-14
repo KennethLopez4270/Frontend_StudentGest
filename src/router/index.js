2fa2cef26c8eae70b9e5ce7fd622644ccdfdc0f1
luzticona7 <chaconlu68@gmail.com>
2025-04-14 11:52:43 -0400

    ADDNuevasVistas

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,12 @@ import JustificarAusencias from '../views/justificarAusencias.vue';
 import Reportes from '../views/reportes.vue';
 import ParentDashboard from '../views/ParentDashboard.vue';
 import PasswordRecoveryView from '../views/PasswordRecoveryView.vue';
+import ForumView from '../views/ForumView.vue';
+import AdminReportsView from '../views/AdminReportsView.vue';
+import TaskDetailsView from '../views/TaskDetailsView.vue';
+import TeacherTasksView from '../views/TeacherTasksView.vue';
+import AcademicCalendar from '../views/AcademicCalendar.vue';
+import TeacherPerformanceReports from '../views/TeacherPerformanceReports.vue'; // Nueva vista
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
@@ -18,7 +24,13 @@ const routes = [
   { path: '/justificar-ausencias', name: 'JustificarAusencias', component: JustificarAusencias },
   { path: '/reportes', name: 'Reportes', component: Reportes },
   { path: '/parent-dashboard', name: 'ParentDashboard', component: ParentDashboard },
-  { path: '/recuperar-contrasena', name: 'PasswordRecovery', component: PasswordRecoveryView }
+  { path: '/recuperar-contrasena', name: 'PasswordRecovery', component: PasswordRecoveryView },
+  { path: '/foro', name: 'Forum', component: ForumView },
+  { path: '/admin-reports', name: 'AdminReports', component: AdminReportsView },
+  { path: '/task-details', name: 'TaskDetails', component: TaskDetailsView },
+  { path: '/teacher-tasks', name: 'TeacherTasks', component: TeacherTasksView },
+  { path: '/calendario', name: 'AcademicCalendar', component: AcademicCalendar },
+  { path: '/teacher-performance-reports', name: 'TeacherPerformanceReports', component: TeacherPerformanceReports }, // Nueva ruta
 ];
 
 const router = createRouter({
